09652477eedc9e8b6e79136f3c4aa427071c8b1b
Orest Falchuk <orest@Orests-iMac.local>
2018-08-01 14:55:41 +0300

    Add animation toggle button

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+    $(document).on('click', '.toggle-ballon-animation', function(e) {
+        e.preventDefault();
+        console.log('click');
+        $('.balloon-animation-wrapper').toggle();
+    });
+});
